2fff4ad57bd1c4879bf47f352f55bf26fbbf6e56
kojoluh <dordoedelivery@gmail.com>
2023-07-27 21:42:31 +0000

    first commit, repo initialization

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from 'next/image';
+import { useStateProvider } from "@/context/StateContext";
 
 function onboarding() {
-  return <div>onboarding</div>;
+  const [{ userInfo }] = useStateProvider();
+
+  return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
+    <div className="flex items-center justify-center gap-2">
+      <Image src="/whatsapp.gif" alt='whatsapp' height={300} width={300} />
+    </div>
+    <h2 className="text-2xl">Create your profile</h2>
+    <div className="flex gap-6 mt-6">
+      <div className="flex flex-col items-center justify-center mt-5 gap2">
+        Hi {userInfo?.name}
+      </div>
+    </div>
+  </div>;
 }
 
 export default onboarding;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import AuthRoutes from './routes/AuthRoutes.js';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', AuthRoutes);
+
+const PORT = process.env.PORT || 3005;
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
