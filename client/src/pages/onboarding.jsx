4f738e2c0eaa45bc80246e7499d8d404b0a895a4
kojoluh <dordoedelivery@gmail.com>
2023-07-30 23:56:11 +0000

    added components for onboarding photo feature

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,9 +1,14 @@
-import React from "react";
 import Image from 'next/image';
 import { useStateProvider } from "@/context/StateContext";
+import Input from "@/components/common/Input";
+import Avatar from '@/components/common/Avatar';
+import React, { useState } from "react";
 
 function onboarding() {
   const [{ userInfo }] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name || '');
+  const [about, setAbout] = useState('');
+  const [image, setImage] = useState('/default_avatar.png');
 
   return <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
     <div className="flex items-center justify-center gap-2">
@@ -12,10 +17,14 @@ function onboarding() {
     <h2 className="text-2xl">Create your profile</h2>
     <div className="flex gap-6 mt-6">
       <div className="flex flex-col items-center justify-center mt-5 gap2">
-        Hi {userInfo?.name}
+        <Input name ="Display Name" state={name} setState={setName} label />
+        <Input name="About" state={about} setState={setAbout} label />
+      </div>
+      <div>
+        <Avatar type="xl" image={image} setImage={setImage} />
       </div>
     </div>
-  </div>;
+  </div>
 }
 
 export default onboarding;
